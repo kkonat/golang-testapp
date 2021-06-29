6431f05e077b884989d2c3d141049b79d20a5043
kkonat <presto@presto.biz.pl>
2021-06-29 19:59:52 +0200

    add images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,20 +11,19 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	// mux := pat.New()
-
-	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
 	mux := chi.NewRouter()
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
-	//mux.Use(middleware.Logger)
+	mux.Use(middleware.Logger)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
